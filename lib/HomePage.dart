3f301d1c3d93d73c644f1a89414caeb21fd62cb4
Raj Mandloii <rajmandloi01.01@gmail.com>
2023-04-21 17:01:21 +0530

    minor ui changes

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -24,69 +24,72 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text("Employee App"),
       ),
-      body: StreamBuilder<List<Employee>>(
-        stream: _employeeBloc.employeeListStream,
-        builder: (context, AsyncSnapshot<List<Employee>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator();
-          }
-          return ListView.builder(
-            itemCount: snapshot.data!.length,
-            itemBuilder: (context, index) {
-              return Card(
-                elevation: 5,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(20),
-                      child: Text(
-                        '${snapshot.data![index].id}.',
-                        style: const TextStyle(
-                          fontSize: 18.0,
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: StreamBuilder<List<Employee>>(
+          stream: _employeeBloc.employeeListStream,
+          builder: (context, AsyncSnapshot<List<Employee>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            }
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 5,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(20),
+                        child: Text(
+                          '${snapshot.data![index].id}.',
+                          style: const TextStyle(
+                            fontSize: 18.0,
+                          ),
                         ),
                       ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            '${snapshot.data![index].name}',
-                            style: const TextStyle(
-                              fontSize: 16.0,
+                      Container(
+                        padding: const EdgeInsets.all(20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '${snapshot.data![index].name}',
+                              style: const TextStyle(
+                                fontSize: 16.0,
+                              ),
                             ),
-                          ),
-                          Text(
-                            '₹ ${snapshot.data![index].salary}',
-                            style: const TextStyle(
-                              fontSize: 16.0,
+                            Text(
+                              '₹ ${snapshot.data![index].salary}',
+                              style: const TextStyle(
+                                fontSize: 16.0,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        _employeeBloc.employeeSalaryIncrement
-                            .add(snapshot.data![index]);
-                      },
-                      icon: const Icon(Icons.thumb_up),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        _employeeBloc.employeeSalaryDecrement
-                            .add(snapshot.data![index]);
-                      },
-                      icon: const Icon(Icons.thumb_down),
-                    ),
-                  ],
-                ),
-              );
-            },
-          );
-        },
+                      IconButton(
+                        onPressed: () {
+                          _employeeBloc.employeeSalaryIncrement
+                              .add(snapshot.data![index]);
+                        },
+                        icon: const Icon(Icons.thumb_up),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          _employeeBloc.employeeSalaryDecrement
+                              .add(snapshot.data![index]);
+                        },
+                        icon: const Icon(Icons.thumb_down),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
